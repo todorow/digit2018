607f3712644b92bad6d4ac248fc086cd0d9d902c
vtodev <viktortodev@gmail.com>
2018-12-20 00:45:50 +0100

    removed staff

--- src/model/donor.ts
+++ src/model/donor.ts
@@ -1,6 +1,6 @@
 import {User} from './user';
 import {Problem} from './Problem';
-import {Event} from './Event';
+
 
 export class Donor extends User {
   private problemsId: Array<number>;
@@ -11,11 +11,10 @@ export class Donor extends User {
     this.problemsId = [];
   }
 
+
   public setProblemId(problemId: Problem) {
     this.problemsId.push(problemId.id);
   }
 
-  public setEventId(eventId: Event) {
-    this.eventsId.push(eventId.id);
-  }
+
 }
