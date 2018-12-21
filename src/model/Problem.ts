9e0694fe4c4e5c462bc22bc76ded38be03a6e5d4
Nenad-Krstevski <krstevski.nenad.96@gmail.com>
2018-12-21 01:16:00 +0100

    Add mock-problems

--- src/model/Problem.ts
+++ src/model/Problem.ts
@@ -1,7 +1,6 @@
 export class Problem {
   id: string;
   pictureUrl: string;
-  name: string;
   description: string;
   category: string;
   status: number;
---
+++ src/model/mock/mock-problems.ts
@@ -0,0 +1,15 @@
+
+import {Problem} from "../Problem";
+
+export let PROBLEMS: Problem[]=[
+  {
+    id: '41.98883,21.42164',
+    pictureUrl: 'assets/events/1200px-Stray_dogs_crosswalk.jpg',
+    description: 'Трајче за оваа зима нема доволно дрва, на трајче му се потребни уште 3 кубика дрва.↵број за контакт 072 552-***',
+    category: 'Човек',
+    status: -1,
+    itemsNeeded: ["3 кубика дрва"],
+    privacy: true
+  }
+
+];
